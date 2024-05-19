4e617e1ea613d88ce7036476b2b641ccd8b50740
Sharath <sharathj116@gmail.com>
2024-05-19 12:21:21 +0530

    avoided auth service

--- server/src/app/useCases/auth/userAuth.ts
+++ server/src/app/useCases/auth/userAuth.ts
@@ -1,16 +1,14 @@
 
 import { UserRepository } from "../../../app/repositories/userReppository"
 import { User } from "../../../entities/user"
-import {AuthServiceInterface} from "../../../app/services/authServiceInterface"
+
 export class AuthUseCase {
     constructor(private userRepository: UserRepository) {}
 
     async register(  username: string,  email: string,  password: string   ): Promise<boolean> {
         
-      const encryptedPassword = await AuthServiceInterface.encryptPassword(
-          password
-      )
-        return await this.userRepository.createUser(username, email, encryptedPassword)
+      
+        return await this.userRepository.createUser(username, email, password)
     }
 
     async login(email: string, password: string): Promise<User | null> {
