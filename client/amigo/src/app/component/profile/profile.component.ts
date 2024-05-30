01b0dede85ba058fb852b69e7e2e3e94527e6a2e
Sharath <sharathj116@gmail.com>
2024-05-30 10:27:17 +0530

    delete and edit icon addded

--- client/amigo/src/app/component/profile/profile.component.ts
+++ client/amigo/src/app/component/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environment';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-profile',
@@ -15,6 +16,34 @@ export class ProfileComponent implements OnInit {
   posts: Post[] = [];
 
   constructor(private _http: HttpClient) {}
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: [
+      '<i class="fa fa-arrow-left"></i>',
+      '<i class="fa fa-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
 
   ngOnInit(): void {
     console.log('hello');
