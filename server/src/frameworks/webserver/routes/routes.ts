8074f001cebf3d57f94be2313b8050abc2c3ac87
Sharath <sharathj116@gmail.com>
2024-06-01 13:52:56 +0530

    deleted all code of post from backend

--- server/src/frameworks/webserver/routes/routes.ts
+++ server/src/frameworks/webserver/routes/routes.ts
@@ -1,5 +1,5 @@
 import AuthRouter from "./userAuth"
-import postRoute from "./posts"
+
 import { Application } from "express"
 import ChatRouter from "./chat"
 import userRoute from "./user"
@@ -9,7 +9,7 @@ import adminRouter from "./admin"
 const routes = (app: Application) => {
 
     app.use("/api/user-auth", AuthRouter())
-    app.use("/api/post",postRoute())
+
     app.use("/api/chat",ChatRouter())
     app.use("/api/user", userRoute())
     app.use("/api/admin", adminRouter())
